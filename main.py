3158e0c6a0f56729340934e0497ae5cace0aef38
Tiqur <trevorbrage02@gmail.com>
2021-05-09 00:28:04 -0700

    Refactored download function and formatting

--- main.py
+++ main.py
@@ -19,31 +19,7 @@ api_secret = os.getenv('API_SECRET')
 client = Client(api_key, api_secret)
 
 # Get data for each token
-for token in watchlist:
-    
-    print(f"Downloading {token} historical data...")
-    historical_data = client.get_historical_klines(token, time_interval, time_duration)
-
-    # Extract OHLVC from data
-    for ohlcv in historical_data:
-        [
-            open_time,
-            open,
-            high,
-            low,
-            close,
-            volume,
-            close_time,
-            quote_asset_volume,
-            num_of_trades,
-            taker_buy_base_asset_volume,
-            taker_buy_base_quote_asset_volume,
-            ignore
-        ] = ohlcv
-        
-        
-
-
 current_time = time.time()
-dhd.download_historical_data(client, 'DOGEUSDT', time_interval, current_time - DAY_SEC * 20, current_time)
+for token in watchlist:
+    dhd.download_historical_data(client, token, time_interval, current_time - DAY_SEC * 5, current_time)
 
--- scripts/download_historical_data.py
+++ scripts/download_historical_data.py
@@ -20,6 +20,24 @@ def download_historical_data(client, token, interval, time_start, time_end):
         print(f"{date_start.strftime('%b %d %Y %H:%M:%S')} -> {date_end.strftime('%b %d %Y %H:%M:%S')}")
         historical_data = client.get_historical_klines(token, interval, str(start), str(end))
 
+        # Extract OHLVC from data
+        for ohlcv in historical_data:
+            [
+                open_time,
+                open,
+                high,
+                low,
+                close,
+                volume,
+                close_time,
+                quote_asset_volume,
+                num_of_trades,
+                taker_buy_base_asset_volume,
+                taker_buy_base_quote_asset_volume,
+                ignore
+            ] = ohlcv
+
+
         # Insert data to db
         time_start += DAY_SEC
 
